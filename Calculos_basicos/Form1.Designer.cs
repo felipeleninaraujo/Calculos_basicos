38992a626d76931324b51794ce1ac419f1080f3e
Felipe <Felipe@DESKTOP-1441MMI>
2021-08-26 17:34:54 -0300

    Adicionar arquivos de projeto.

---
+++ Calculos_basicos/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿
+namespace Calculos_basicos
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnSomar = new System.Windows.Forms.Button();
+            this.btnSubtrair = new System.Windows.Forms.Button();
+            this.btnMutiplicar = new System.Windows.Forms.Button();
+            this.btnDividir = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSomar
+            // 
+            this.btnSomar.BackColor = System.Drawing.Color.Transparent;
+            this.btnSomar.Location = new System.Drawing.Point(200, 161);
+            this.btnSomar.Name = "btnSomar";
+            this.btnSomar.Size = new System.Drawing.Size(37, 23);
+            this.btnSomar.TabIndex = 0;
+            this.btnSomar.Text = "+";
+            this.btnSomar.UseVisualStyleBackColor = false;
+            this.btnSomar.Click += new System.EventHandler(this.btnSomar_Click);
+            // 
+            // btnSubtrair
+            // 
+            this.btnSubtrair.BackColor = System.Drawing.Color.Transparent;
+            this.btnSubtrair.Location = new System.Drawing.Point(253, 161);
+            this.btnSubtrair.Name = "btnSubtrair";
+            this.btnSubtrair.Size = new System.Drawing.Size(39, 23);
+            this.btnSubtrair.TabIndex = 1;
+            this.btnSubtrair.Text = "-";
+            this.btnSubtrair.UseVisualStyleBackColor = false;
+            this.btnSubtrair.Click += new System.EventHandler(this.btnSubtrair_Click);
+            // 
+            // btnMutiplicar
+            // 
+            this.btnMutiplicar.BackColor = System.Drawing.Color.Transparent;
+            this.btnMutiplicar.Location = new System.Drawing.Point(200, 203);
+            this.btnMutiplicar.Name = "btnMutiplicar";
+            this.btnMutiplicar.Size = new System.Drawing.Size(37, 23);
+            this.btnMutiplicar.TabIndex = 2;
+            this.btnMutiplicar.Text = "*";
+            this.btnMutiplicar.UseVisualStyleBackColor = false;
+            this.btnMutiplicar.Click += new System.EventHandler(this.btnMutiplicar_Click);
+            // 
+            // btnDividir
+            // 
+            this.btnDividir.BackColor = System.Drawing.Color.Transparent;
+            this.btnDividir.Location = new System.Drawing.Point(253, 203);
+            this.btnDividir.Name = "btnDividir";
+            this.btnDividir.Size = new System.Drawing.Size(39, 23);
+            this.btnDividir.TabIndex = 3;
+            this.btnDividir.Text = "/";
+            this.btnDividir.UseVisualStyleBackColor = false;
+            this.btnDividir.Click += new System.EventHandler(this.btnDividir_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.Color.Ivory;
+            this.btnSair.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSair.Location = new System.Drawing.Point(100, 278);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 23);
+            this.btnSair.TabIndex = 4;
+            this.btnSair.Text = "SAIR";
+            this.btnSair.UseVisualStyleBackColor = false;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.Color.Ivory;
+            this.btnLimpar.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnLimpar.Location = new System.Drawing.Point(200, 278);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 5;
+            this.btnLimpar.Text = "LIMPAR";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(62, 158);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(100, 23);
+            this.txtNum1.TabIndex = 6;
+            this.txtNum1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(65, 204);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(100, 23);
+            this.txtNum2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.LightGray;
+            this.textBox3.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBox3.Location = new System.Drawing.Point(100, 22);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(187, 30);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.Text = "CALCULOS BASICOS";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.Ivory;
+            this.textBox4.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox4.Location = new System.Drawing.Point(9, 158);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(47, 26);
+            this.textBox4.TabIndex = 9;
+            this.textBox4.Text = "Num 1";
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.Color.Ivory;
+            this.textBox5.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox5.Location = new System.Drawing.Point(9, 204);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(50, 26);
+            this.textBox5.TabIndex = 10;
+            this.textBox5.Text = "Num 2";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.BackColor = System.Drawing.Color.White;
+            this.txtResultado.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtResultado.Location = new System.Drawing.Point(118, 78);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(133, 23);
+            this.txtResultado.TabIndex = 11;
+            this.txtResultado.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(140, 107);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(86, 26);
+            this.textBox1.TabIndex = 12;
+            this.textBox1.Text = "RESULTADO";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(360, 306);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnDividir);
+            this.Controls.Add(this.btnMutiplicar);
+            this.Controls.Add(this.btnSubtrair);
+            this.Controls.Add(this.btnSomar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSomar;
+        private System.Windows.Forms.Button btnSubtrair;
+        private System.Windows.Forms.Button btnMutiplicar;
+        private System.Windows.Forms.Button btnDividir;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Calculos_basicos/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculos_basicos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtNum1.Text = "";
+            txtNum2.Text = "";
+            txtResultado.Text = "";
+               
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnSomar_Click(object sender, EventArgs e)
+        {
+            //Declaração das variaveis como double
+            double num1, num2, resultado;
+
+
+            /* num1 e num2 recebe conteudo do textbox
+            (txtnum1 e txtnum2.text)
+            converte.Todouble é nescessario para q seja convertido.
+            o conteudo do textbox para numero.*/
+            num1 = Convert.ToDouble(txtNum1.Text);
+            num2 = Convert.ToDouble(txtNum2.Text);
+
+            //Variavel resultado = recebe conteudo de num1_nume2 e realiza o calculo.
+                resultado = num1 + num2;
+
+            //txtResultado.Text = recebe conteudo da variavel resultado e converte para numero.
+            txtResultado.Text = resultado.ToString();
+        }
+
+        private void btnSubtrair_Click(object sender, EventArgs e)
+        {
+            double num1, num2, resultado;
+
+
+            num1 = Convert.ToDouble(txtNum1.Text);
+            num2 = Convert.ToDouble(txtNum2.Text);
+            resultado = num1 - num2;
+
+            txtResultado.Text = resultado.ToString();
+
+        }
+
+        private void btnDividir_Click(object sender, EventArgs e)
+        {
+            double num1, num2, resultado;
+
+            num1 = Convert.ToDouble(txtNum1.Text);
+            num2 = Convert.ToDouble(txtNum2.Text);
+
+            resultado = num1 / num2;
+
+            txtResultado.Text = resultado.ToString();
+
+
+
+        }
+
+        private void btnMutiplicar_Click(object sender, EventArgs e)
+        {
+            double num1, num2, resultado;
+
+            num1 = Convert.ToDouble(txtNum1.Text);
+            num2 = Convert.ToDouble(txtNum2.Text);
+
+            resultado = num1 * num2;
+
+            txtResultado.Text = resultado.ToString();
+
+
+
+        }
+    }
+}
+ 
